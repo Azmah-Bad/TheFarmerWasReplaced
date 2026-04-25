a7ca58747e81c32e9090f4f54b9b3ef0a04e421f
Hamza <56384031+Azmah-Bad@users.noreply.github.com>
2026-04-25 15:41:47 +0200

    UNLOCKED FUNCTIONS

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+def process_square(x, y):
+    quick_print("Processing square at")
+    # if can_harvest():
+    harvest()
+    # till()
+    plant(Entities.Bush)
+    quick_print("Square processed.")
+
+
+print("Starting farming bot...")
+
 while True:
+    quick_print("Farming...")
 
-	FARM_DIMENSIONS = (get_world_size(), get_world_size())
+    FARM_DIMENSIONS = (get_world_size(), get_world_size())
+    quick_print("Farm dimensions", FARM_DIMENSIONS)
 
-	for row in range(FARM_DIMENSIONS[0]):
-		move(East)
-		for column in range(FARM_DIMENSIONS[1]):
-			move(North)
-			if can_harvest():
-				harvest()
-				till()
-				plant(Entities.Bush)
+    for row in range(FARM_DIMENSIONS[0]):
+        move(East)
+        for column in range(FARM_DIMENSIONS[1]):
+            move(North)
+            process_square(row, column)
